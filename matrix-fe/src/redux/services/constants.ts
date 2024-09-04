f2538c51d0aa92c8e3d911587f873ed54e5c545c
Shubham Bisht <Shubham.Bisht@cantire.com>
2024-09-04 15:59:38 +0530

    updates backend constant

--- matrix-fe/src/redux/services/constants.ts
+++ matrix-fe/src/redux/services/constants.ts
@@ -1,5 +1,6 @@
 export const constants = {
-    BASE_URL: 'http://localhost:5000',
+    // BASE_URL: 'http://localhost:5000',
     //BASE_URL: 'http://ec2-13-234-208-91.ap-south-1.compute.amazonaws.com:9500', //garage
     //BASE_URL: 'http://ecsf002007e3.epam.com:5000', //assist
+    BASE_URL: 'https://matrix-ruddy.vercel.app/'
 }
